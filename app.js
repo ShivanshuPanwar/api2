3054754f279e043ff44ae809030997b64cfcecc5
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2024-02-26 20:39:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');           // access express module
+var app = express();                        // create an object of express module
+var bodyparser=require('body-parser');
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+var student = require('./routes/student');
+
+app.use('/student',student);
+
+app.get('/', function(req,res) {
+    res.send('Welcome to nodejs api express server');
+});
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, async () => {
+    try {
+        console.log('Running server on port 3000');
+    } catch (error) {
+        console.log(error);
+    }
+});
